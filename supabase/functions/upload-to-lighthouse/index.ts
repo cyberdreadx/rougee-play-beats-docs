2ab912751e342b996eadd723790e35676aef5ca8
CyberDread <37160508+rougecoin-project@users.noreply.github.com>
2025-10-15 01:22:37 -0400

    x privy cors

--- supabase/functions/upload-to-lighthouse/index.ts
+++ supabase/functions/upload-to-lighthouse/index.ts
@@ -5,7 +5,7 @@ import { z } from 'https://deno.land/x/zod@v3.22.4/mod.ts';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-wallet-address',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-wallet-address, x-privy-token',
 };
 
 serve(async (req) => {
